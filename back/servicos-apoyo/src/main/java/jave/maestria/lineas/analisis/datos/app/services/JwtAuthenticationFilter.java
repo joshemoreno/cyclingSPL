a2c6087c8f3a8774c552e59f04513a8ae36d19c4
edwintoro <edwindistoro@gmail.com>
2024-11-27 20:58:30 -0500

    se suben cambios en servicios-apoyo

--- back/servicos-apoyo/src/main/java/jave/maestria/lineas/analisis/datos/app/services/JwtAuthenticationFilter.java
+++ back/servicos-apoyo/src/main/java/jave/maestria/lineas/analisis/datos/app/services/JwtAuthenticationFilter.java
@@ -11,8 +11,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -22,6 +24,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     public JwtAuthenticationFilter(JwtService jwtService) {
         this.jwtService = jwtService;
     }
+
+    @Value("#{'${plans}'.split(',')}")private List<String> plans;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -42,11 +46,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                     .getBody();
 
             String username = claims.getSubject();
-            String rol = claims.get("rol", String.class);
+            String plan = claims.get("plan", String.class);
+
+            if (plan == null || !this.plans.contains(plan)) {    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                response.getWriter().write("Acceso denegado: falta el claim requerido.");    return;}
 
             if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                        username, null, jwtService.getAuthoritiesFromRoles(rol));
+                        username, null, jwtService.getAuthoritiesFromRoles(plan));
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
                 SecurityContextHolder.getContext().setAuthentication(authentication);
